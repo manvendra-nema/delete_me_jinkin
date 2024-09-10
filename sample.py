e612a903f17dc3a2b31c015201a39dd6d013b22a
manvendra-nema <manvendra.nema1@gamil.com>
2024-09-10 14:49:20 +0530

    file upload

---
+++ sample.py
@@ -0,0 +1,6 @@
+def print():
+    return "function #DEF"
+
+def write(abc):
+    return (f"write {abc} ")
+print(write("this is written "))
---
+++ test_sample.py
@@ -0,0 +1,20 @@
+import sample
+import unittest
+
+
+# Unit test class
+class TestFunctions(unittest.TestCase):
+
+    def test_print(self):
+        # Test the print function
+        result = sample.print()
+        self.assertEqual(result, "function #DEF")
+
+    def test_write(self):
+        # Test the write function with sample input
+        result = samplewrite("this is written")
+        self.assertEqual(result, "write this is written ")
+
+# This allows the test to run when the script is executed
+if __name__ == '__main__':
+    unittest.main()
